3331095f8a428b6d9051da3e9a2a3367e3fafd45
artursreg2 <artursreg2@gmail.com>
2023-11-14 13:40:59 +0200

    Update spele.py

--- spele.py
+++ spele.py
@@ -1,7 +1,15 @@
 from tkinter import*
 from tkinter import messagebox
-manslogs=Tk()
+manslogs = Tk()
 manslogs.title("TicTacToe")
+
+def btnClick(button):
+    global speletajsX, count
+    if button["text"]==""and speletajsX==True:
+        button["text"]="X"
+        speletajsX=False
+        count+=1
+    
 
 btn1= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
 btn2= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
@@ -12,16 +20,19 @@ btn6= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24")
 btn7= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
 btn8= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
 btn9= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn10= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
 
-btn1.grid(row=0, column=0)
-btn2.grid(row=0, column=0)
-btn3.grid(row=0, column=0)
-btn4.grid(row=0, column=0)
-btn5.grid(row=0, column=0)
-btn6.grid(row=0, column=0)
-btn7.grid(row=0, column=0)
-btn8.grid(row=0, column=0)
-btn9.grid(row=0, column=0)
+btn1.grid(row=0, column = 0)
+btn2.grid(row=0, column = 1)
+btn3.grid(row=0, column = 2)
+btn4.grid(row=1, column = 0)
+btn5.grid(row=1, column = 1)
+btn6.grid(row=1, column = 2)
+btn7.grid(row=2, column = 0)
+btn8.grid(row=2, column = 1)
+btn9.grid(row=2, column = 2)
+
+manslogs.mainloop()
 
 
 
