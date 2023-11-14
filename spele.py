61da51c2ae14e74da2f2d9d87a4d0460a41dbccf
artursreg2 <artursreg2@gmail.com>
2023-11-14 13:39:53 +0200

    Pirmā stunda
    Pirmā stunda

---
+++ spele.py
@@ -0,0 +1,52 @@
+from tkinter import*
+from tkinter import messagebox
+manslogs=Tk()
+manslogs.title("TicTacToe")
+
+btn1= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn2= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn3= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn4= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn5= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn6= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn7= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn8= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+btn9= Button(manslogs, text = " ", width = 6, height = 3, font = ("Helvica, 24"))
+
+btn1.grid(row=0, column=0)
+btn2.grid(row=0, column=0)
+btn3.grid(row=0, column=0)
+btn4.grid(row=0, column=0)
+btn5.grid(row=0, column=0)
+btn6.grid(row=0, column=0)
+btn7.grid(row=0, column=0)
+btn8.grid(row=0, column=0)
+btn9.grid(row=0, column=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+manslogs.mainloop()
