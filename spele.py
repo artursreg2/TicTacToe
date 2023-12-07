d84f1dbd4900d663778001cb6e41b67a8a2e000f
artursreg2 <artursreg2@gmail.com>
2023-12-07 14:40:01 +0200

    TicTacToe

--- spele.py
+++ spele.py
@@ -28,21 +28,26 @@ def checkWinner():
     winner = False
     if (btn1["text"] == "X" and btn2["text"] == "X" and btn3["text"] == "X" or 
         btn4["text"] == "X" and btn5["text"] == "X" and btn6["text"] == "X" or 
-        btn7["text"] == "X" and btn8["text"] == "X" and btn9["text"] == "X"):
+        btn7["text"] == "X" and btn8["text"] == "X" and btn9["text"] == "X" or 
+        btn1["text"] == "X" and btn4["text"] == "X" and btn7["text"] == "X" or 
+        btn2["text"] == "X" and btn5["text"] == "X" and btn8["text"] == "X" or 
+        btn3["text"] == "X" and btn6["text"] == "X" and btn9["text"] == "X" or 
+        btn1["text"] == "X" and btn5["text"] == "X" and btn9["text"] == "X" or 
+        btn3["text"] == "X" and btn5["text"] == "X" and btn7["text"] == "X"):
         winner = True
         messagebox.showinfo("TicTacToe", "Spēlētājs X ir uzvarētājs")
-        return 0
-        winner=True
         disableButtons()
     elif (btn1["text"] == "O" and btn2["text"] == "O" and btn3["text"] == "O" or 
           btn4["text"] == "O" and btn5["text"] == "O" and btn6["text"] == "O" or 
-          btn7["text"] == "O" and btn8["text"] == "O" and btn9["text"] == "O"):
+          btn7["text"] == "O" and btn8["text"] == "O" and btn9["text"] == "O" or 
+          btn1["text"] == "O" and btn4["text"] == "O" and btn7["text"] == "O" or 
+          btn2["text"] == "O" and btn5["text"] == "O" and btn8["text"] == "O" or 
+          btn3["text"] == "O" and btn6["text"] == "O" and btn9["text"] == "O" or 
+          btn1["text"] == "O" and btn5["text"] == "O" and btn9["text"] == "O" or 
+          btn3["text"] == "O" and btn5["text"] == "O" and btn7["text"] == "O"):
         winner = True
         disableButtons()
         messagebox.showinfo("TicTacToe", "Spēlētājs O ir uzvarētājs")
-        return 0
-        winner = True
-        disableButtons()    
     elif count == 9 and winner == False:
         messagebox.showinfo("TicTacToe", "Neizšķirts")
        
@@ -56,22 +61,20 @@ def reset():
             btn7.config(state=NORMAL)
             btn8.config(state=NORMAL)
             btn9.config(state=NORMAL)
-            btn1["text"]=""
-            btn2["text"]=""
-            btn3["text"]=""
-            btn4["text"]=""
-            btn5["text"]=""
-            btn6["text"]=""
-            btn7["text"]=""
-            btn8["text"]=""
-            btn9["text"]=""
+            btn1["text"]=" "
+            btn2["text"]=" "
+            btn3["text"]=" "
+            btn4["text"]=" "
+            btn5["text"]=" "
+            btn6["text"]=" "
+            btn7["text"]=" "
+            btn8["text"]=" "
+            btn9["text"]=" "
             global winner, count, speletajsX
             winner=False
             count=0
             speletajsX=True
             return 0
-
-
 
 def disableButtons(): #spēle beidzas, pogas izslēgtas
             btn1.config(state=DISABLED)
@@ -85,15 +88,15 @@ def disableButtons(): #spēle beidzas, pogas izslēgtas
             btn9.config(state=DISABLED)
 
 
-btn1 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="black", command=lambda: btnClick(btn1))
-btn2 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn2))
-btn3 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn3))
-btn4 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn4))
-btn5 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn5))
-btn6 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn6))
-btn7 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn7))
-btn8 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn8))
-btn9 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24), command=lambda: btnClick(btn9))
+btn1 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="blue", command=lambda: btnClick(btn1))
+btn2 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="white", command=lambda: btnClick(btn2))
+btn3 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="blue", command=lambda: btnClick(btn3))
+btn4 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="white", command=lambda: btnClick(btn4))
+btn5 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="blue", command=lambda: btnClick(btn5))
+btn6 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="white", command=lambda: btnClick(btn6))
+btn7 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="blue", command=lambda: btnClick(btn7))
+btn8 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="white", command=lambda: btnClick(btn8))
+btn9 = Button(manslogs, text=" ", width=6, height=3, font=("Helvetica", 24),bd=7,bg="blue", command=lambda: btnClick(btn9))
 
 btn1.grid(row=0, column=0)
 btn2.grid(row=0, column=1)
@@ -111,14 +114,23 @@ opcijas=Menu(galvenaIzvelne,tearoff=False)#mazā izvēlne
 galvenaIzvelne.add_cascade(label="Opcijas",menu=opcijas) #lejupkrītošais saraksts
 opcijas.add_command(label="Jauna spēle",command=reset)
 opcijas.add_command(label="Iziet",command=manslogs.quit)
+
 def infoLogs():
     jaunsLogs=Toplevel()
     jaunsLogs.title('Info par programmu')
-    jaunsLogs.geometry("300x300")
-    apraksts=Label(jaunsLogs,text='Spēles noteikumi'"OK")
+    jaunsLogs.geometry("550x150")
+    apraksts=Label(jaunsLogs,text="SPĒLES NOTEIKUMI")
     apraksts.grid(row=0,column=0)
+    apraksts=Label(jaunsLogs,text="Jūs esat X , jūsu draugs (vai dators šajā gadījumā) ir O .")
+    apraksts.grid(row=1,column=0)
+    apraksts=Label(jaunsLogs,text=" Spēlētāji pēc kārtas liek savas atzīmes tukšos laukumos.")
+    apraksts.grid(row=2,column=0)
+    apraksts=Label(jaunsLogs,text="Uzvar pirmais spēlētājs, kurš iegūst 3 savas atzīmes pēc kārtas (augšup, lejup, šķērsām vai pa diagonāli).")
+    apraksts.grid(row=3,column=0)
+    apraksts=Label(jaunsLogs,text="Kad visi 9 laukumi ir aizpildīti, spēle ir beigusies.")
+    apraksts.grid(row=4,column=0)
     return 0
-
+  
 galvenaIzvelne.add_command(label="Par programmu",command=infoLogs) # pievieno mazajai izvēlnei
 
 
